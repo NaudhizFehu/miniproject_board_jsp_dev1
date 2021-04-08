6551829f61cbcaac045ef73939dbe935695eb876
jade <jeonghyon.lee@gmail.com>
2021-04-09 00:45:02 +0900

    delete.do commit

---
+++ src/article/command/DeleteArticleHandler.java
@@ -0,0 +1,32 @@
+package article.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import article.service.ArticleContentNotFoundException;
+import article.service.ArticleNotFoundException;
+import article.service.DeleteArticleService;
+import mvc.command.CommandHandler;
+
+public class DeleteArticleHandler implements CommandHandler{
+	
+	private DeleteArticleService deleteService
+	= new DeleteArticleService();
+
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		String noVal = req.getParameter("no");
+		int articleNum = Integer.parseInt(noVal);
+		
+		try {
+				deleteService.delete(articleNum);
+				return "/WEB-INF/view/deleteSuccess.jsp";
+		} catch (ArticleNotFoundException e) {
+				res.sendError(HttpServletResponse.SC_NOT_FOUND);
+				return null;
+		} catch (ArticleContentNotFoundException e) {
+			res.sendError(HttpServletResponse.SC_NOT_FOUND);
+			return null;
+		}
+	}
+}
--- src/article/dao/ArticleDao.java
+++ src/article/dao/ArticleDao.java
@@ -137,4 +137,14 @@ public class ArticleDao {
 			return pstmt.executeUpdate();
 		}
 	}
+	
+	public void delete(Connection conn, int no) throws SQLException{
+			try(PreparedStatement pstmt = 
+					conn.prepareStatement(
+							"delete from aricle where article_no = ?")){
+					pstmt.setInt(1, no);
+					pstmt.executeUpdate();
+					return;
+			}
+	}
 }
